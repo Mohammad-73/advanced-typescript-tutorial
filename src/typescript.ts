285c85bb80c98e909570512f5d558bc7b641e2a5
Mohammad-73 <m.khalili7310@gmail.com>
2026-06-05 18:22:31 +0330

    Introduce three important ts configuration

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,5 +1,29 @@
 // Add quokka extension to vscode and run it with ctrl + shift + P.
 
-const add = (a: number, b: number) => a + b;
+// ----- Setting up the development environment -----
+// const add = (a, b) => a + b;
+// const add = (a: number, b: number) => a + b; // TS Friendly
 
-console.log(add(5, 2));
+// console.log(add(5, 2));
+
+// ----- Introduce two important typescript configurations -----
+// const multiply = (a:number, b:number) => a * b;
+
+// const getLength = (str: string) => str.length;
+
+// ----------
+
+// const getLength = (str: string | undefined) => {
+//   if (str === undefined) {
+//     return 0;
+//   }
+//   return str.length;
+// }; // TS Friendly
+
+// console.log(getLength(undefined));
+
+// ----------
+
+// const enrolledCoursed = ["typescript", "websocket", "next.js"];
+
+// console.log(enrolledCoursed[3].toLowerCase());
