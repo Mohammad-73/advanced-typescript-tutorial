df3ef18b730bc960cdc7b403d1deb6132cffe26a
Mohammad Khalili <m.khalili7310@gmail.com>
2026-06-15 12:20:46 +0330

    Use the number type when defining index signature

---
+++ src/02/08.ts
@@ -0,0 +1,20 @@
+// // Number keys converted to string on "runtime"
+// const courseModules = {
+//   1: "Introduction to Javascript",
+//   2: "Advanced Javascript",
+// };
+
+// type CourseModule = {
+//   [key: number]: string;
+// }; // Another reason to don't use this syntax
+
+// const number: Array<number> = [];
+
+// console.log(Object.keys(courseModules));
+// console.log(typeof []);
+
+// const studentIds = [1001, 1002, 1003, 1004, 1005];
+// console.log(studentIds[0]);
+// console.log(studentIds["0"]);
+
+// console.log(Object.keys(studentIds));
