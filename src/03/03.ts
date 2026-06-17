71b52ba9ce183f4734288c998392549d7478c562
Mohammad Khalili <m.khalili7310@gmail.com>
2026-06-17 10:21:14 +0330

    Define separate variables for different types

---
+++ src/03/03.ts
@@ -0,0 +1,24 @@
+// let courseId = "CLB450";
+// console.log(typeof courseId);
+// if(typeof(courseId) === "string"){
+//     const s = courseId;
+// }
+
+// courseId = 54120;
+// if(typeof(courseId) === "number"){
+//     const s = courseId;
+// }
+// console.log(typeof courseId);
+
+// let courseSerial = 54120;
+
+// const courseId = "CLB450"
+
+// -------------
+
+// let courseId = "CLB450";
+// console.log(typeof courseId);
+// {
+//   let courseId = 54120;
+//   console.log(typeof courseId);
+// }
