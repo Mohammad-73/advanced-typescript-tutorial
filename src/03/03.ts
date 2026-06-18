4d202bd1d018b7c56f99ee4efd874b78971e2cff
Mohammad-73 <m.khalili7310@gmail.com>
2026-06-18 13:28:43 +0330

    How is the type of a variable extracted in TS

--- src/03/03.ts
+++ src/03/03.ts
@@ -1,24 +1,53 @@
-// let courseId = "CLB450";
-// console.log(typeof courseId);
-// if(typeof(courseId) === "string"){
-//     const s = courseId;
+// interface Course {
+//   title: string;
+//   status: "published" | "draft" | "archived";
 // }
 
-// courseId = 54120;
-// if(typeof(courseId) === "number"){
-//     const s = courseId;
+// function getCoursesByStatus(status: "published" | "draft" | "archived") {
+//   // fetch courses here by status logic
 // }
-// console.log(typeof courseId);
 
-// let courseSerial = 54120;
+// // let courseStatus = "published";
+// const courseStatus = "published";
+// getCoursesByStatus(courseStatus);
 
-// const courseId = "CLB450"
+// const course = {
+//   title: "Javascript",
+//   status: "published" as const,
+// };
 
-// -------------
+// course.status = "sssss";
 
-// let courseId = "CLB450";
-// console.log(typeof courseId);
-// {
-//   let courseId = 54120;
-//   console.log(typeof courseId);
-// }
+// ------------
+
+// const importanceLevel = [1, 2, 3] as const;
+// importanceLevel[0] = 10;
+
+// const locationT = [5.22, 6.65] as const;
+// locationT[2] = 41;
+
+// ------------
+
+// const course = {
+//   title: "Next.js and React Real-time System",
+//   duration: 30,
+//   students: [{ name: "John", id: 1, address: { city: "Tehran" } }],
+// } as const;
+
+// course.students[0].address.city = "Shiraz";
+
+// ------------
+
+// type Point = [number, number];
+
+// const tehran = {
+//   location: [-73.75, 42.65],
+// } satisfies Record<string, Point>;
+
+// tehran.location[0] = 10;
+
+// const karaj: Record<string, Point> = {
+//   location: [73, 74],
+// };
+
+// karaj.location2 = [47, 51];
