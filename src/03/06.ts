16d0ab812f805875e3f0fad8c745c37d0c57bd34
Mohammad Khalili <m.khalili7310@gmail.com>
2026-06-21 09:12:23 +0330

    Context is another important factor in type detection in TS

---
+++ src/03/06.ts
@@ -0,0 +1,18 @@
+// type ButtonType = "regular" | "icon" | "circle";
+
+// declare function renderButton(type: ButtonType): void;
+
+// renderButton("icon");
+
+// // let iconButton = "icon";
+// const iconButton = "icon"
+// renderButton(iconButton);
+
+// ------------
+
+// declare function setColor(color: readonly [number, number, number]): void;
+
+// setColor([255, 0, 0]);
+
+// const redColor = [255, 0, 0] as const;
+// setColor(redColor);
