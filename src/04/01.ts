b22af4824b34ceba89699263eab7fdd95d3c3bdd
Mohammad-73 <m.khalili7310@gmail.com>
2026-06-26 15:30:56 +0330

    The recommended approach for designing types in TS

---
+++ src/04/01.ts
@@ -0,0 +1,25 @@
+// // type ThemeConfig = {
+// //   darkMode: boolean;
+// //   fontSize: number;
+// //   primaryColor: string;
+// // };
+
+// // type PartialThemeConfig = {
+// //   darkMode?: boolean;
+// //   fontSize?: number;
+// //   primaryColor?: string;
+// // };
+
+// const defaultThemeConfig = {
+//   darkMode: false,
+//   fontSize: 16,
+//   primaryColor: "#333",
+//   secondaryColor: "#666",
+// };
+
+// const applyTheme = (configs?: Partial<typeof defaultThemeConfig>) => {
+//   const allConfigs = { ...defaultThemeConfig, ...configs };
+//   console.log(allConfigs);
+// };
+
+// applyTheme({ darkMode: true });
