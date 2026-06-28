8db6775bedfa2944a5c026d4c565c02ca10f7dd9
Mohammad Khalili <m.khalili7310@gmail.com>
2026-06-28 10:20:40 +0330

    Never use null or undefined directly when defining types

---
+++ src/04/03.ts
@@ -0,0 +1,15 @@
+// type NullableUser = {
+//   id: number;
+//   name: string;
+// } | null;
+
+// const getUserInfo = (user: NullableUser) => {
+//   if (user) {
+//     console.log(user.name);
+//   }
+// };
+
+// getUserInfo({
+//   id: 1,
+//   name: "ali",
+// });
