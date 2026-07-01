4ed35024ec3cdee3a5e764f46f75742eaf25f2b9
Mohammad Khalili <m.khalili7310@gmail.com>
2026-07-01 10:42:11 +0330

    Never define consecutive parameter with the same type

---
+++ src/04/06.ts
@@ -0,0 +1,35 @@
+// createModal("the process is done", 300, 200, 300, "fade-in");
+
+// function createModal(
+//   content: string,
+//   width: number,
+//   height: number,
+//   animationDuration: number,
+//   animationType: string,
+// ): void {}
+
+// interface ModalContent {
+//   content: string;
+// }
+
+// interface ModalDimensions {
+//   width: number;
+//   height: number;
+// }
+
+// interface ModalAnimation {
+//   animationDuration: number;
+//   animationType: string;
+// }
+
+// interface ModalProps extends ModalContent, ModalDimensions, ModalAnimation {}
+
+// declare function createModal(params: ModalProps): void;
+
+// createModal({
+//   content: "the process is done",
+//   width: 300,
+//   height: 200,
+//   animationDuration: 300,
+//   animationType: "fade-in",
+// });
